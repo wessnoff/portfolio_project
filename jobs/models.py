50784639e109fcb722bf2abc84b13a1c6f4f7f73
Mikhail Trus <wessnoff@gmail.com>
2018-06-03 14:30:44 +0300

    Jobs app up and running, Add postgresqldb

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    """ Description the Jobs model"""
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
